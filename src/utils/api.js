ab17240408f6299ebe2074958ca5fd905a7186f5
Ishwar Dhakad <ishwardhakad779@gmail.com>
2026-07-24 03:27:39 +0530

    Fix template literal in api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api'
 
 async function request(path, options = {}) {
-  const res = await fetch('${API_BASE_URL}${path}' , {
+  const res = await fetch(`${API_BASE_URL}${path}`, {
     headers: { 'Content-Type': 'application/json' },
     ...options,
   })
